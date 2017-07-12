7491046955da20ae89241211261fb0d798595275
David Zuidema <dczuidema@gmail.com>
2017-07-11 22:32:15 -0400

    Adding test to empty class to fix build error

--- src/test/java/com/davidzuidema/web/payment/PaymentServiceImplTest.java
+++ src/test/java/com/davidzuidema/web/payment/PaymentServiceImplTest.java
@@ -1,9 +1,18 @@
 package com.davidzuidema.web.payment;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PaymentServiceImplTest {
 
+	@InjectMocks
+	PaymentServiceImpl subject;
+
+	@Test
+	public void scheduleSinglePayment() throws Exception {
+		subject.scheduleSinglePayment(new SinglePaymentRequest());
+	}
 }
