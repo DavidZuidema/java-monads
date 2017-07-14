a2c0dfe8c637bba5c96358cd73cc7140b5fb7f03
David Zuidema <dczuidema@gmail.com>
2017-07-13 22:33:49 -0400

    Added another payment rule for business restrictions

--- src/main/java/com/davidzuidema/web/security/PaymentRules.java
+++ src/main/java/com/davidzuidema/web/security/PaymentRules.java
@@ -1,5 +1,7 @@
 package com.davidzuidema.web.security;
 
+import java.time.Duration;
+import java.time.LocalDate;
 import java.util.function.Function;
 
 import com.davidzuidema.web.monad.Result;
@@ -16,5 +18,14 @@ public class PaymentRules {
 		return request -> request.getCustomerId().equals(customer.getId())
 				? Result.success(request)
 				: Result.error("Unauthorized Action");
+	}
+
+	public static Result<ScheduleSinglePaymentRequest> isAfterFreeze(
+			ScheduleSinglePaymentRequest request) {
+		LocalDate today = LocalDate.now();
+		long daysInAdvance = Duration.between(today.atStartOfDay(), request.getDate().atStartOfDay()).toDays();
+		return daysInAdvance < 2
+				? Result.error("Must schedule payments at least TWO days in advance")
+				: Result.success(request);
 	}
 }
--- src/test/java/com/davidzuidema/web/security/PaymentRulesTest.java
+++ src/test/java/com/davidzuidema/web/security/PaymentRulesTest.java
@@ -7,6 +7,8 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
 
+import java.time.LocalDate;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,6 +19,9 @@ import com.davidzuidema.web.payment.ScheduleSinglePaymentRequest;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PaymentRulesTest {
+
+	private static final LocalDate TOMORROW = LocalDate.now().plusDays(1);
+	private static final LocalDate TWO_DAYS_FROM_NOW = LocalDate.now().plusDays(2);
 
 	Customer customer;
 
@@ -39,6 +44,25 @@ public class PaymentRulesTest {
 		ScheduleSinglePaymentRequest request = create(new ScheduleSinglePaymentRequest(), r -> r.setCustomerId("1"));
 
 		Result<ScheduleSinglePaymentRequest> result = PaymentRules.hasAccess(customer).apply(request);
+
+		assertThat(result, succeededAndValue(is(sameInstance(request))));
+	}
+
+	@Test
+	public void isAfterFreeze_withPaymentDateOfTomorrow_returnsError() throws Exception {
+		ScheduleSinglePaymentRequest request = create(new ScheduleSinglePaymentRequest(), r -> r.setDate(TOMORROW));
+
+		Result<ScheduleSinglePaymentRequest> result = PaymentRules.isAfterFreeze(request);
+
+		assertThat(result, failedAndError(is("Must schedule payments at least TWO days in advance")));
+	}
+
+	@Test
+	public void isAfterFreeze_withPaymentDateOfTwoDaysFromNow_returnsSuccess() throws Exception {
+		ScheduleSinglePaymentRequest request = create(new ScheduleSinglePaymentRequest(),
+				r -> r.setDate(TWO_DAYS_FROM_NOW));
+
+		Result<ScheduleSinglePaymentRequest> result = PaymentRules.isAfterFreeze(request);
 
 		assertThat(result, succeededAndValue(is(sameInstance(request))));
 	}
