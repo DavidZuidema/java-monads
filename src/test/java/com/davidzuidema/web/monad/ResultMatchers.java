7b4757f63110ca3c2c2a8b8680449503c258a35f
David Zuidema <dczuidema@gmail.com>
2017-07-13 22:29:27 -0400

    Hamcrest Matchers for the Result Monad

---
+++ src/test/java/com/davidzuidema/web/monad/ResultMatchers.java
@@ -0,0 +1,75 @@
+package com.davidzuidema.web.monad;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
+
+public class ResultMatchers {
+	public static <A> Matcher<Result<A>> succeededAndValue(Matcher<A> successValueMatcher) {
+		return new TypeSafeDiagnosingMatcher<Result<A>>() {
+
+			@Override
+			public void describeTo(Description description) {
+				description
+						.appendText("A Successful Result<> whose value")
+						.appendText(" ")
+						.appendDescriptionOf(successValueMatcher);
+			}
+
+			@Override
+			protected boolean matchesSafely(Result<A> item, Description mismatch) {
+				if (item.failed()) {
+					mismatch
+							.appendText("was a Failed Result<> whose error message is \"")
+							.appendText(item.error())
+							.appendText("\"");
+					return false;
+				}
+				A successValue = item.extract();
+				if (!successValueMatcher.matches(successValue)) {
+					mismatch
+							.appendText("was a Successful Result<> whose value")
+							.appendText(" ");
+					successValueMatcher.describeMismatch(successValue, mismatch);
+					return false;
+				}
+				return true;
+			}
+
+		};
+	}
+
+	public static <A> Matcher<Result<A>> failedAndError(Matcher<String> errorMessageMatcher) {
+		return new TypeSafeDiagnosingMatcher<Result<A>>() {
+
+			@Override
+			public void describeTo(Description description) {
+				description
+						.appendText("A Failed Result<> whose error message")
+						.appendText(" ")
+						.appendDescriptionOf(errorMessageMatcher);
+			}
+
+			@Override
+			protected boolean matchesSafely(Result<A> item, Description mismatch) {
+				if (item.succeeded()) {
+					mismatch
+							.appendText("was a Successful Result<> whose value is")
+							.appendText(" ")
+							.appendValue(item.extract());
+					return false;
+				}
+				String errorMessage = item.error();
+				if (!errorMessageMatcher.matches(errorMessage)) {
+					mismatch
+							.appendText("was a Failed Result<> whose error message")
+							.appendText(" ");
+					errorMessageMatcher.describeMismatch(errorMessage, mismatch);
+					return false;
+				}
+				return true;
+			}
+
+		};
+	}
+}
